cedb93533245b2dadc1f244f972c5f891ff63a41
skqhs7276 <skqhs7276@gmail.com>
2024-07-30 15:20:47 +0900

    feat: modfiy user infomation api

--- backend/src/main/java/S11P12A708/A708/domain/user/service/UserService.java
+++ backend/src/main/java/S11P12A708/A708/domain/user/service/UserService.java
@@ -1,26 +1,29 @@
 package S11P12A708.A708.domain.user.service;
 
 import S11P12A708.A708.domain.auth.exception.InvalidPasswordException;
+import S11P12A708.A708.domain.auth.service.AuthService;
 import S11P12A708.A708.domain.user.exception.UserNotFoundException;
 import S11P12A708.A708.domain.user.entity.User;
 import S11P12A708.A708.domain.user.repository.UserRepository;
 import S11P12A708.A708.domain.user.request.ChangePwRequest;
+import S11P12A708.A708.domain.user.request.ChangeUserRequest;
 import S11P12A708.A708.domain.user.response.UserInfo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Slf4j
 @Service
 public class UserService {
     private final PasswordEncoder bCryptPasswordEncoder;
     private final UserRepository userRepository;
+    private final AuthService authService;
 
-    public UserService(PasswordEncoder bCryptPasswordEncoder, UserRepository userRepository) {
+    public UserService(PasswordEncoder bCryptPasswordEncoder, UserRepository userRepository,
+                       AuthService authService) {
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
         this.userRepository = userRepository;
+        this.authService = authService;
     }
 
     public UserInfo getUserInfoByUserId(Long userId) throws RuntimeException {
@@ -28,8 +31,17 @@ public class UserService {
         return new UserInfo(user);
     }
 
-    public Optional<User> getUserByEmail(String email) throws RuntimeException {
-        return userRepository.findByEmail(email);
+    public UserInfo updateUserInfo(Long userId, ChangeUserRequest req) {
+        User user = userRepository.findById(userId).orElseThrow(UserNotFoundException::new);
+
+        if (!req.getNickName().equals(user.getNickname()) && authService.checkNickName(req.getNickName())) {
+            user.setNickname(req.getNickName());
+        }
+        user.setGitId(req.getGitId());
+        user.setRepo(req.getRepo());
+        user.setDescription(req.getDescription());
+        userRepository.save(user);
+        return new UserInfo(user);
     }
 
     public boolean changePassword(Long userId, ChangePwRequest req) {
