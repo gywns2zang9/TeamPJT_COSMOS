d72bafeccc3df6efdd5218e1f8e4dcdf71a1b4ae
gywns2zang9 <gywns2zang9@gmai.com>
2024-07-31 17:40:48 +0900

    style: 코드 스타일 수정

--- frontend/src/store/auth.js
+++ frontend/src/store/auth.js
@@ -1,4 +1,8 @@
 // src/store/auth.js
+import { deleteRequest, get, patch, post } from '../api/api.js'
+import useStore from './index.js'
+
+const BASE_URL = useStore.getState().BASE_URL;
 
 // 액세스 토큰을 가져오는 함수
 export const getAccessToken = () => {
@@ -15,10 +19,28 @@ export const getUserInfo = () => {
     return JSON.parse(localStorage.getItem("userInfo"));
 };
 
-// 로그인 여부를 확인하는 함수
-export const isLogin = () => {
-    const accessToken = localStorage.getItem("accessToken");
-    return !!accessToken;
+// 로그인 요청 함수
+export const login = async ({ email, password }) => {
+
+    try {
+        const url = `${BASE_URL}/auth/login`;
+        const data = {
+            email,
+            password
+        };
+        const responseData = await post(url, data);
+        const { accessToken, refreshToken, userInfo } = responseData;
+
+        localStorage.setItem("accessToken", accessToken);
+        localStorage.setItem("refreshToken", refreshToken);
+        localStorage.setItem("userInfo", JSON.stringify(userInfo));
+        console.log("로그인 성공")
+        return { accessToken, refreshToken, userInfo };
+
+    } catch (err) {
+        console.log("로그인 실패->", err);
+        throw err;
+    }
 };
 
 // 로그아웃 함수
@@ -30,3 +52,10 @@ export const logout = () => {
     // 홈 페이지로 리다이렉트
     window.location.href = "/";
 };
+
+// 로그인 여부를 확인하는 함수
+export const isLogin = () => {
+    const accessToken = localStorage.getItem("accessToken");
+    return !!accessToken;
+};
+
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,7 +1,7 @@
 import { create } from 'zustand';
 
 const useStore = create((set) => ({
-    BASE_URL: 'http://localhost:8080/api/',
+    BASE_URL: 'http://localhost:8080',
 }))
 
 export default useStore;
