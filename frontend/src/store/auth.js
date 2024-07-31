b1410d21d7c8472c83da64424f73ed5199cb28a7
gywns2zang9 <gywns2zang9@gmai.com>
2024-07-31 16:47:18 +0900

    style: 코드 스타일 수정

--- frontend/src/store/auth.js
+++ frontend/src/store/auth.js
@@ -1,5 +1,32 @@
+// src/store/auth.js
+
+// 액세스 토큰을 가져오는 함수
+export const getAccessToken = () => {
+    return localStorage.getItem("accessToken");
+};
+
+// 리프레시 토큰을 가져오는 함수
+export const getRefreshToken = () => {
+    return localStorage.getItem("refreshToken");
+};
+
+// 사용자 정보를 가져오는 함수
+export const getUserInfo = () => {
+    return JSON.parse(localStorage.getItem("userInfo"));
+};
+
 // 로그인 여부를 확인하는 함수
-export const isLoggedIn = () => {
+export const isLogin = () => {
     const accessToken = localStorage.getItem("accessToken");
     return !!accessToken;
-}
+};
+
+// 로그아웃 함수
+export const logout = () => {
+    localStorage.removeItem("accessToken");
+    localStorage.removeItem("refreshToken");
+    localStorage.removeItem("userInfo");
+
+    // 홈 페이지로 리다이렉트
+    window.location.href = "/";
+};
