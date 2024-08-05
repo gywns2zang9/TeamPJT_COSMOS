b7b296a4b1c251b41275f775a39d3c21ca2b0ef1
gywns2zang9 <gywns2zang9@gmai.com>
2024-08-05 10:46:19 +0900

    feat: conference-code 입력

--- frontend/src/components/cenference/Code.jsx
+++ frontend/src/components/cenference/Code.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Code(props) {
-  return <div>Codespace</div>;
+function Code() {
+  const [code, setCode] = useState(`print("hello")\nprint("cosmos")\nprint("A708")`);
+
+  // 코드 입력 핸들러
+  const handleCodeChange = (event) => {
+    setCode(event.target.value);
+  };
+
+  return (
+    <div>
+      <textarea
+        value={code}
+        onChange={handleCodeChange}
+        style={{ width: "100%", height: "400px", resize: "none" }}
+      />
+    </div>
+  );
 }
 
 export default Code;
