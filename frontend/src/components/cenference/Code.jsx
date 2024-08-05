ddd82b4298c686eb9f8dc22d172f219d55d7f4bb
gywns2zang9 <gywns2zang9@gmai.com>
2024-08-05 11:13:34 +0900

    design: 코드 입력출력 화면구성

--- frontend/src/components/cenference/Code.jsx
+++ frontend/src/components/cenference/Code.jsx
@@ -9,12 +9,18 @@ function Code() {
   };
 
   return (
-    <div>
+    <div style={{ display: 'flex', flexDirection: 'column', height: '100%',padding: '10px' }}>
       <textarea
         value={code}
         onChange={handleCodeChange}
-        style={{ width: "100%", height: "400px", resize: "none" }}
+        style={{ flex: 4, border: '1px solid black', padding: '10px' }}
       />
+      <div style={{ display: 'flex', flex: 1, width: '100%', marginTop: '5px', gap: '5px' }}>
+        <textarea style={{ flex: 1, padding: '10px', border: '1px solid black' }} />
+        <div style={{ flex: 1, border: '1px solid black', padding: '10px', boxSizing: 'border-box' }}>
+          result
+        </div>
+      </div>
     </div>
   );
 }
