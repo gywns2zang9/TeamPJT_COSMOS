6645789327ceee83bab68807dca08737ea6e90dd
JHS <ghtjd1551@naver.com>
2024-08-06 10:10:36 +0900

    fix:useEffect for page

--- frontend/src/views/GroupDetailView.jsx
+++ frontend/src/views/GroupDetailView.jsx
@@ -1,34 +1,65 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import GroupDetailInfo from '../components/group/groupDetailInfo.jsx';
 import SideBar from '../components/group/sideBar.jsx';
 import '../css/group/groupDetail.css';
+import useGroupStore from "../store/group.js";
 
 const GroupDetailView = () => {
-    const location = window.location.pathname;
+    const { pageId, groupId } = useParams();
 
+    const location = window.location.pathname;
+    const [loadedType, setLoadedType] = useState('main');
+    const getFile = useGroupStore(state => state.getFile);
     let type;
-    if (location.includes('/main')) {
-        type = 'main';
-    } else if (location.includes('/overview')) {
-        type = 'overview';
-    } else if (location.includes('/code/')) {
-        type = 'code';
-    } else if (location.includes('/time-overview/')) {
-        type = 'time-overview';
-    } else {
-        type = 'normal';
+
+    useEffect(() => {
+        console.log(groupId, pageId);
+        setLoadedType(getFile({ groupId, fileId:pageId }));
+    }, [getFile]);
+
+    switch (loadedType) {
+        case 'Main':
+            type = 'main';
+            break;
+        case 'Code':
+            type = 'code';
+            break;
+        case 'OverView':
+            type = 'overview';
+            break;
+        case 'TimeOverView':
+            type = 'time-overview';
+            break;
+        case 'Normal':
+            type = 'normal';
+            break;
+        default:
+            type = 'normal';
     }
 
-    const { pageId, groupId, } = useParams();
-    
+    if (location) {
+        if (location.includes('/main')) {
+            type = 'main';
+        } else if (location.includes('/overview')) {
+            type = 'overview';
+        } else if (location.includes('/code/')) {
+            type = 'code';
+        } else if (location.includes('/time-overview/')) {
+            type = 'time-overview';
+        } else {
+            type = 'normal';
+        }
+    }
+
+
     return (
         <div id="group-detail-info">
             <div id="sidebar">
-            <SideBar groupId={groupId}/>
+                <SideBar groupId={groupId} />
             </div>
             <div id='group-detail-info-page'>
-            <GroupDetailInfo pageId={pageId} type={type} groupId={groupId}/>
+                <GroupDetailInfo pageId={pageId} type={type} groupId={groupId} />
             </div>
         </div>
     );
