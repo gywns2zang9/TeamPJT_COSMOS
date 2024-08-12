e7a9ea2146c422caee1a9f79e7c28b1f8246d0b7
kwlgur0416@gmail.com <kwlgur0416@gmail.com>
2024-08-12 23:43:38 +0900

    feat : disconnect bef connection in openvidu session

--- backend/src/main/java/S11P12A708/A708/domain/conference/controller/RtcController.java
+++ backend/src/main/java/S11P12A708/A708/domain/conference/controller/RtcController.java
@@ -1,5 +1,7 @@
 package S11P12A708.A708.domain.conference.controller;
 
+import S11P12A708.A708.domain.auth.annotation.AuthUser;
+import S11P12A708.A708.domain.auth.request.AuthUserDto;
 import io.openvidu.java.client.*;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,7 +12,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @RestController
 public class RtcController {
@@ -25,6 +29,8 @@ public class RtcController {
 
     private final static String customSessionId = "customSessionId";
 
+    private final Map<Long, String> userConnections = new ConcurrentHashMap<>();
+
     @PostConstruct
     public void init() {
         this.openvidu = new OpenVidu(OPENVIDU_URL, OPENVIDU_SECRET);
@@ -36,9 +42,7 @@ public class RtcController {
     @PostMapping("/sessions/teams/{teamId}")
     public ResponseEntity<String> initializeSession(@PathVariable("teamId") String teamId)
             throws OpenViduJavaClientException, OpenViduHttpException {
-        Map<String, Object> params = Map.of(customSessionId, teamId);
-        SessionProperties properties = SessionProperties.fromJson(params).build();
-        Session session = openvidu.createSession(properties);
+        Session session = createSession(teamId);
         return new ResponseEntity<>(session.getSessionId(), HttpStatus.OK);
     }
 
@@ -48,16 +52,42 @@ public class RtcController {
      * @return The Token associated to the Connection
      */
     @PostMapping("/sessions/teams/{teamId}/connections")
-    public ResponseEntity<String> createConnection(@PathVariable("teamId") String sessionId,
+    public ResponseEntity<String> createConnection(@AuthUser AuthUserDto authUser,
+                                                   @PathVariable("teamId") String sessionId,
                                                    @RequestBody(required = false) Map<String, Object> params)
-            throws OpenViduJavaClientException, OpenViduHttpException {
-        Session session = openvidu.getActiveSession(sessionId);
-        if (session == null) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            throws Exception {
+
+        // 이전에 해당 유저가 접근한 커넥션이 열려있다면 닫는다.
+        if (userConnections.containsKey(authUser.getId())) {
+            removeConnection(authUser, sessionId);
         }
+
+        Session session = createSession(sessionId);
+
         ConnectionProperties properties = ConnectionProperties.fromJson(params).build();
         Connection connection = session.createConnection(properties);
+        userConnections.put(authUser.getId(), connection.getConnectionId());
+
         return new ResponseEntity<>(connection.getToken(), HttpStatus.OK);
+    }
+
+    private Session createSession(String teamId) throws OpenViduJavaClientException, OpenViduHttpException {
+        Map<String, Object> params = Map.of(customSessionId, teamId);
+        SessionProperties properties = SessionProperties.fromJson(params).build();
+        Session session = openvidu.createSession(properties);
+        return session;
+    }
+
+    private void removeConnection(AuthUserDto authUser, String sessionId) throws Exception {
+        Session session = openvidu.getActiveSession(sessionId);
+        if (session == null) throw new Exception("session 업음");
+
+        List<Connection> activeConnections = session.getActiveConnections();
+        for (Connection connection : activeConnections) {
+            if (connection.getConnectionId().equals(userConnections.get(authUser.getId()))) {
+                session.forceDisconnect(connection); // 세션에 입장한 사용자 연결 끊기
+            }
+        }
     }
 
 }
