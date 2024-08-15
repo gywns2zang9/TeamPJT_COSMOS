1e08307c5e0928cc653fe3a6b09af9cfaad96870
JHS <ghtjd1551@naver.com>
2024-08-16 07:38:50 +0900

    .

--- frontend/src/modals/CreateProblemModal.jsx
+++ frontend/src/modals/CreateProblemModal.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Modal, Button, Form, Spinner } from "react-bootstrap";
 import useGroupStore from "../store/group";
+import axios from 'axios';
 
 function CreateProblemModal({
   show,
@@ -9,10 +10,10 @@ function CreateProblemModal({
   studyId,
   existingProblems,
 }) {
-  const [problemSite, setProblemSite] = useState("");
   const [problemNumber, setProblemNumber] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const createProblem = useGroupStore((state) => state.createProblem);
+  const maxProblemNumber = 100000
 
   useEffect(() => {
     if (show) {
@@ -23,8 +24,25 @@ function CreateProblemModal({
   const handleSubmit = async (e) => {
     e.preventDefault();
 
+    // 문제 번호가 빈 문자열일 경우
     if (!problemNumber.trim()) {
       alert("문제 번호를 입력하세요.");
+      return;
+    }
+
+    // 문제 번호가 자연수가 아닌 경우
+    if (!/^[1-9]\d*$/.test(problemNumber)) {
+      alert("올바른 수를 입력해주세요.\n"); // 자연수 형식이 아닌 경우
+      return;
+    }
+
+    if (maxProblemNumber === null) {
+      alert("문제 수를 가져오는 중입니다. 잠시 후 다시 시도해주세요.");
+      return;
+    }
+
+    if (parseInt(problemNumber, 10) < 1000 || parseInt(problemNumber, 10) > maxProblemNumber) {
+      alert(`문제 번호는 1000 이상 ${maxProblemNumber} 이하이어야 합니다.`);
       return;
     }
 
@@ -37,6 +55,7 @@ function CreateProblemModal({
     }
 
     setIsLoading(true); // 로딩 상태 시작
+
     try {
       await createProblem({ groupId, problemNumber, studyId });
       window.location.reload();
